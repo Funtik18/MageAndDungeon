3565da16d269c17cb41110c9a1fef6faba33bff1
Igor Davidovich <sir.funtik18@gmail.com>
2021-02-05 23:23:43 +0700

    Update
    Добавление несколькох префабов и ресурсов.

---
+++ Assets/Scripts/Enviroment/Floor/Floor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+	[Header("Grid")]
+	public int gridWidth = 1;
+	public int gridHeight = 1;
+
+	[Header("Tile")]
+	public FloorTile floorPrefab;
+	public Vector2 offset;
+	public float floorWidth = 5;
+	public float floorHeight = 5;
+
+	[Header("Collider")]
+	public Vector3 colliderSize;
+	private GameObject floorCollider;
+
+
+	[HideInInspector] public List<FloorTile> floorTiles = new List<FloorTile>();
+
+	[ContextMenu("Floor Initialization")]
+	public void Initialization()
+	{
+		DestroyAllChildren(transform);
+		InstantiateFloor();
+	}
+
+	private void InstantiateFloor()
+	{
+		for(int i = 0; i < gridWidth; i++)
+		{
+			for(int j = 0; j < gridHeight; j++)
+			{
+				Transform t = Instantiate(floorPrefab, transform).transform;
+				t.localPosition = new Vector3(offset.x + floorWidth * i, 0, offset.y - floorHeight * j);
+
+				floorTiles.Add(t.GetComponent<FloorTile>());
+			}
+		}
+
+		floorCollider = new GameObject("_collider");
+		floorCollider.transform.SetParent(transform);
+		BoxCollider box = floorCollider.AddComponent<BoxCollider>();
+		box.size = colliderSize;
+	}
+
+	private void DestroyAllChildren(Transform parent)
+	{
+		floorCollider = null;
+		floorTiles.Clear();
+		for(int i = parent.childCount - 1; i >= 0 ; i--)
+		{
+			DestroyImmediate(parent.GetChild(i).gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/LightBallTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class LightBallTest : MonoBehaviour
+{
+	public Transform target;
+
+	private void Update()
+	{
+		if(target == null) return;
+
+		if(Input.GetMouseButton(0))
+		{
+			RaycastHit hit;
+			Vector3 pos = Input.mousePosition;
+			pos = Camera.main.ScreenToWorldPoint(pos);
+			Ray ray = new Ray(pos, Vector3.down);
+
+			if(Physics.Raycast(ray, out hit))
+			{
+				target.position = hit.point;
+			}
+			//Debug.LogError(Input.mousePosition);
+			//Vector3 pos = Camera.main.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+			//pos.y = 3;
+			
+		}
+	}
+}
