890d8f21df30592affa39650ced94179d487af37
hazker <zov.zon@gmail.com>
2021-02-05 18:49:40 +0700

    Work in progress

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public List<Sprite> sprites;
+
+    Rigidbody rb;
+    Transform target; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+        target = FindObjectOfType<Mag>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(,rb.velocity.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    Joystick joyStick;
+    JoyButton joyButton;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        joyStick = FindObjectOfType<Joystick>();
+        joyButton = FindObjectOfType<JoyButton>();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        rb.velocity = new Vector3(joyStick.Horizontal * speed * Time.deltaTime, joyStick.Vertical * speed * Time.deltaTime, rb.velocity.z);
+    }
+}
