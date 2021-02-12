f0cebb43dceab11cb1469ac4221015a5c9a00baf
Igor Davidovich <sir.funtik18@gmail.com>
2021-02-12 19:17:31 +0700

    Update GUI

---
+++ Assets/Scripts/UI/LevelsMap/Windows/WindowUpgrades/WindowUpgrades.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class WindowUpgrades : MonoBehaviour
+{
+    private Fader fader;
+    public Fader Fader
+    {
+        get
+        {
+            if(fader == null)
+            {
+                fader = GetComponent<Fader>();
+            }
+            return fader;
+        }
+    }
+
+
+    [ContextMenu("OpenWindow")]
+    public void OpenWindow()
+    {
+        Fader.CanvasGroup.alpha = 1;
+        Fader.CanvasGroup.interactable = true;
+        Fader.CanvasGroup.blocksRaycasts = true;
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(gameObject);
+#endif
+    }
+    [ContextMenu("CloseWindow")]
+    public void CloseWindow()
+    {
+        Fader.CanvasGroup.alpha = 0;
+        Fader.CanvasGroup.interactable = false;
+        Fader.CanvasGroup.blocksRaycasts = false;
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(gameObject);
+#endif
+    }
+}
