176829855ef3a3aa342a8c3690676992eda35d5a
hazker <zov.zon@gmail.com>
2021-02-24 21:14:58 +0700

    FPSCounter

---
+++ Assets/FPSCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int avgFrameRate;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+    }
+
+    void OnGUI()
+    {
+            GUI.Label(new Rect(20, 40, 80, 20), avgFrameRate.ToString());  
+    }
+}
